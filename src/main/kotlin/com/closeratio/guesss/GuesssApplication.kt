f7263ba437c58fb162763617457602f4aaf1914f
Rick <richardgale90@gmail.com>
2025-04-03 23:53:20 +0100

    Added Spring starter

---
+++ src/main/kotlin/com/closeratio/guesss/GuesssApplication.kt
@@ -0,0 +1,11 @@
+package com.closeratio.guesss
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GuesssApplication
+
+fun main(args: Array<String>) {
+	runApplication<GuesssApplication>(*args)
+}
